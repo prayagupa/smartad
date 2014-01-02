f2de918367f9f44eefbdc9e8b233535b6a05d2ce
iPeayag <prayag.upd@gmail.com>
2014-01-02 17:34:43 +0545

    ADD getJson method to handle json url request

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -14,31 +14,39 @@ import play.api.libs.json.JsValue
 object Application extends Controller {
 
   def index =  Action {
-      val start = java.lang.System.currentTimeMillis()
-      implicit val timeout = Timeout(50000 milliseconds)
-      val url = "http://localhost:8443/DasTest/esReport?clientId=2000&reportingBasis=ServiceDate&reportingTo=2013-10-31&reportingFrom=2012-11-01&comparisonFrom=2011-11-01&comparisonTo=2012-10-31&report=qualityMetric&reportingPaidThrough=2013-10-31&comparisonPaidThrough=2012-10-31&phiCSDate=11-10-2008&phiCEDate=10-31-2013&isParticipation=y&program_type=Wellness&program_type=Activity%20Tracker"
-      Logger.info("url : {}",url)
-      val responseFuture = WS.url(url).get()
-      val future = responseFuture map {
-        response => (response.json \\ "reporting")
-      }
-      val result = Await.result(future, timeout.duration).asInstanceOf[List[play.api.libs.json.JsObject]]
 
-      val size = result.size
-      var jsonVal: JsValue = null
-      var json: String = ""
-      val json_ = result.foreach(jsonObject =>
-        jsonObject.values.foreach(jsValue => {
-            Logger.info("jsvalue : "+jsValue)
-            jsonVal = jsValue
-            json = jsValue.toString()
-        }
-        )
+      Ok(views.html.index("Request Url"))
+
+  }
+
+  def getJson = Action { implicit request =>
+    val url = request.queryString.get("url").flatMap(_.headOption).getOrElse("")
+
+    val start = java.lang.System.currentTimeMillis()
+    implicit val timeout = Timeout(70000 milliseconds)
+
+    Logger.info("url : {}",url)
+    val responseFuture = WS.url(url).get()
+    val future = responseFuture map {
+      response => (response.json \\ "reporting")
+    }
+    val result = Await.result(future, timeout.duration).asInstanceOf[List[play.api.libs.json.JsObject]]
+
+    val size = result.size
+    var jsonVal: JsValue = null
+    var json: String = ""
+    val json_ = result.foreach(jsonObject =>
+      jsonObject.values.foreach(jsValue => {
+        Logger.info("jsvalue : "+jsValue)
+        jsonVal = jsValue
+        json = jsValue.toString()
+      }
       )
-      val end = java.lang.System.currentTimeMillis()
-      Logger.info("size : {}", size.toString)
-      Logger.info("Time taken : {} seconds", ((end - start)/1000).toString)
-      Ok(views.html.index(size, json.toString))
+    )
+    val end = java.lang.System.currentTimeMillis()
+    Logger.info("size : {}", size.toString)
+    Logger.info("Time taken : {} seconds", ((end - start)/1000).toString)
+    Ok(views.html.json(jsonVal))
   }
 
 }
