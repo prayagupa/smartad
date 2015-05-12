1422ebbaae8e2077e56ba64d950adc31fec624a7
prayagupd <prayag.upd@gmail.com>
2015-05-12 15:42:00 -0500

    add custom mapwritable for stripe approach

---
+++ smartad-backend/alsobought-prediction-stripes/src/main/scala/CustomMapWritable.scala
@@ -0,0 +1,24 @@
+import org.apache.hadoop.io.{MapWritable, Writable, WritableComparable, IntWritable}
+import scala.collection.JavaConversions._
+
+/**
+ * Created by prayagupd
+ * on 5/12/15.
+ */
+
+class CustomMapWritable extends MapWritable {
+
+  //def this() = this()
+
+  override
+  def toString() : String = {
+    val keys = this.keySet()
+    var string = ""
+    for(writable <- keys) {
+      string+=s"(${writable}, ${this.get(writable)}),"
+    }
+    string = "[" + string.substring(0, string.length-1) + "]"
+
+    string
+  }
+}
